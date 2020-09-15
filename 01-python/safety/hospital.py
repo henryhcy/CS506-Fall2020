cf540890a667883e051c57b34cdd0bbbab8553da
henryhcy <henryhcy@bu.edu>
2020-09-14 22:59:32 -0400

    modify hospital adding parameters: width, height function default: width >= 4 height >= 3
    Second change

--- 01-python/safety/hospital.py
+++ 01-python/safety/hospital.py
@@ -1,3 +1,15 @@
-def draw_hospital():
-    print("hospital not found")
-    return
+def draw_hospital(width = 4, height = 3):
+
+    if width < 4 or height < 3:
+        raise Exception("width >= 4, height >= 3")
+    
+    indent = width - 4
+    indent = indent // 2
+    
+    print("-"*width)
+    print(" "*indent + " || ")
+    print(" "*indent + "=  =")
+    print(" "*indent + " || ")
+    for h in range(height - 3):
+        print()
+    print("-"*width)
