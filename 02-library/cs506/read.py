4299aee0274b5ddc49f0f5b661bfb46aabc186ce
henryhcy <henryhcy@bu.edu>
2020-09-16 22:44:19 -0400

    not finishied

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -3,4 +3,15 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    res = []
+    with open(csv_file_path) as f:
+        for line in f:
+            line = line.rstrip()
+            line = line.split(",")
+            line = [int(x) if x.isdigit() else x.strip("\"") for x in line ]
+            res.append(line)
+    return res
+#
+# if __name__ == '__main__':
+#     path = "/Users/macbook/Downloads/college/GRAD/CS506/git_learn/CS506-Fall2020/02-library/tests/test_files/dataset_2.csv"
+#     read_csv(path)
--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -1,5 +1,16 @@
 def euclidean_dist(x, y):
-    raise NotImplementedError()
+    n = len(x)
+    if n == 0:
+        raise ValueError()
+    else:
+        res_dist = 0
+        for i in range(n):
+            dist = abs(x[i] - y[i])
+            dist_n = dist**n
+            res_dist+=dist_n # sum
+        n_root = 1/(n) # nth root
+        res = res_dist**(n_root)
+        return res
 
 def manhattan_dist(x, y):
     raise NotImplementedError()
@@ -11,3 +22,7 @@ def cosine_sim(x, y):
     raise NotImplementedError()
 
 # Feel free to add more
+# if __name__ == '__main__':
+#     x = [0,0]
+#     y = [0,1]
+#     euclidean_dist(x,y)
